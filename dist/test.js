66dcff6e28ec0fb3a10d2ec75980881d210e9f9b
Naruedon-mu <marknarudon@gmail.com>
2025-09-22 14:15:47 +0700

    update

---
+++ dist/test.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Utils_1 = require("./Utils");
+if ((0, Utils_1.add)(1, 2) === 3) {
+    console.log(0);
+}
+else {
+    console.log(1);
+}
